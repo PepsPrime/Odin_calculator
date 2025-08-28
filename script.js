e6c3a578a0f9d80764eeb03e24e4f3f32c54d162
PepsPrime <184280384+PepsPrime@users.noreply.github.com>
2025-08-28 10:35:17 +0200

    Implemented calculation logic
    Based on a 3 elements array (num1,operator, num2); will look for better visual

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function multiply(a,b) {
 function divide(a,b) {
     if (b==0) {
         alert("Error : Dividing by 0.....")
-        return 
+
     }
     let result = a / b
     return result
@@ -29,37 +29,41 @@ function divide(a,b) {
 
 // Calling the proper function
 
-function operate(operator,a,b) {
+function operate(a,operator,b) {
     
     switch(operator) {
         case "+":
-            add(a,b)
+            return add(a,b)
             break
         case '-':
-            subtract(a,b)
+            return subtract(a,b)
             break
         case '*':
-            multiply(a,b)
+            return multiply(a,b)
             break
         case '/':
-            divide(a,b)
+            return divide(a,b)
             break
         default:
-            alert("Non-authorized operator")
-            return 'Error'
+           alert('Error on operate function')
             
     }
 }
 
-let num1 = null;
-let num2 = null;
-let operator = ""
+// Array containing 1st operand, operator and 2nd operand
+let stack = [null,"",null];
+let result;
+let operatorIndex = 0;
+// set to true when a calculation is done
+let equalityCheck = false;
+
 
 function clearData() {
-    num2 = null;
-    num1 = null;
-    operator = "";
+
+    stack = [null, "", null];
     displayText.textContent = "0";
+    operatorIndex = 0;
+    equalityCheck = false;
 }
 
 let btn = document.querySelectorAll("button");
@@ -68,7 +72,7 @@ for(i=0; i<btn.length; i++) {
     btn[i].addEventListener("click", (e) => {
 
         let clickedButton = e.target.textContent;
-
+        
         // Check if a number, an operator or the clear button was clicked
         switch (clickedButton) {
             case "0":
@@ -81,20 +85,65 @@ for(i=0; i<btn.length; i++) {
             case "7":
             case "8":
             case "9":
-                if (displayText.textContent == "0") {
+                // pressed digit when display = 0 or sign equal was pressed
+                // erase the display 
+                if ((displayText.textContent == "0") || (equalityCheck)) {
                     displayText.textContent = clickedButton;
+                    equalityCheck = false;
+                    
                 } else {
                     displayText.textContent += clickedButton;
                 }
-
                 break
             case "+":
             case "-":
             case "/":
             case "*":
+                // 
+                equalityCheck = false;
+                // check if last input is an operator
+                // if so, swaps it
+                if (operators.includes(displayText.textContent.slice(-1))) {
+
+                    displayText.textContent = displayText.textContent.replace(displayText.textContent.slice(-1),clickedButton)
+                    stack[1] = clickedButton
+                    break
+
+                // when an operator key is pressed while there's already one in the stack
+                // calculate the first operation and set it to index 0 of stack
+                } else if (stack[1].length != 0) {
+                    
+                    
+                    operatorIndex = displayText.textContent.indexOf(stack[1]);
+                    stack[2] = Number(displayText.textContent.substring(operatorIndex+1))
+                    stack[0] = operate(stack[0],stack[1],stack[2])
+                    displayText.textContent = "" + stack[0] + clickedButton
+
+                    stack[1] = clickedButton;
+                    stack[2] = null;
+
+                } else {
+                    stack[0] = Number(displayText.textContent)
+                    displayText.textContent += clickedButton
+                    stack[1] = clickedButton;
+                    
+                }
+                break
+
             case "=":
-                num1 = Number(displayText.textContent)
-                displayText.textContent += clickedButton;
+                if ((stack[0] === null) || (stack[1] == '')) {
+                    alert("Error : missing arguments, try again")
+                    clearData()
+
+                } else {
+                    operatorIndex = displayText.textContent.indexOf(stack[1]);
+                    stack[2] = Number(displayText.textContent.substring(operatorIndex+1))
+                    result = operate(stack[0],stack[1],stack[2]) + ""
+                    clearData()
+                    displayText.textContent = result + ''
+                    equalityCheck = true;
+                    
+                }
                 break
             case "Clear":
                 clearData()
